54f8fb91a56761ae04bf64775539f796ac73f9c9
SHORYASHOKANDA <shoryashokanda@gmail.com>
2024-03-10 22:10:31 +0530

    new

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -10,14 +10,14 @@ export default function Footer() {
           href="https://github.com/SHORYASHOKANDA"
           className="font-bold hover:underline underline-offset-4"
         >
-          C
+          SHORYA SHOKANDA
         </a>
       </p>
       <span className="inline-flex gap-4 sm:ml-auto sm:mt-0 mt-4 items-center justify-center sm:justify-start">
         <span className="text-sm font-medium leading-none">
           Give it a ⭐ on
         </span>
-        <a href="https://github.com/sohomofficial/imaginify">
+        <a href="https://github.com/SHORYASHOKANDA">
           <Button size={"icon"}>
             <Github className="h-6 w-6" />
           </Button>
